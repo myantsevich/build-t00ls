8c1def9175965c908c7b782534d595b2d44e5e63
Anastasiya Britikova <32018826+BritikovaA@users.noreply.github.com>
2020-04-06 19:57:23 +0300

    Update HelloWorldServiceImpl.java

--- helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
+++ helloworld-project/helloworld-ws/src/main/java/org/jboss/as/quickstarts/wshelloworld/HelloWorldServiceImpl.java
@@ -67,9 +67,6 @@ public class HelloWorldServiceImpl implements HelloWorldService {
         if (names == null || names.isEmpty()) {
             return "Anonymous!";
         }
-     int k = 20;
-        while(k > 0)
-            k--;
         final StringBuilder nameBuilder = new StringBuilder();
         for (int i = 0; i < names.size(); i++) {
 
