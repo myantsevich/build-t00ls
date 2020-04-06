4a470db3511946680ff02ac4743dee012343cdbc
Maxim Filipovich <maxim_filipovich@epam.com>
2020-04-07 02:19:41 +0300

    broken file5

--- helloworld-project/helloworld-ws/src/test/java/org/jboss/as/quickstarts/wshelloworld/Client.java
+++ helloworld-project/helloworld-ws/src/test/java/org/jboss/as/quickstarts/wshelloworld/Client.java
@@ -29,7 +29,9 @@ import java.util.List;
  */
 public class Client implements HelloWorldService {
     private HelloWorldService helloWorldService;
-
+while(true){
+    i=++
+}
     /**
      * Default constructor
      *
